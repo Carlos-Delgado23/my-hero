8d425a05793684f6c445e5fc297fcd8276a2bd97
unknown <carlos.delgado.dev@gmail.com>
2020-04-26 13:19:48 -0500

    conditionals and class bindings

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes'
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
-  
   heroes = HEROES;
 
   hero: Hero = {
     id: 1,
-    name: 'Windstorm'
+    name: 'Windstorm',
   };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
